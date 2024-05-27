a1b0c498f3a008291455c83660341d1c47876447
Nicolas <ndebacher@gmail.com>
2024-05-26 21:30:56 -0300

    Showing the results in a scrollview card

--- Frufrufruit-ui/src/ResultView.jsx
+++ Frufrufruit-ui/src/ResultView.jsx
@@ -1,22 +1,59 @@
-import Navbar from "./Navbar";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import Navbar from "./Navbar";
+import Papa from "papaparse";
 
 export default function ResultsView() {
     const location = useLocation();
     const file = location.state?.file;
-
-    // Create a download link for the file if it exists
+    const [data, setData] = useState([]);
     const url = file ? window.URL.createObjectURL(file) : null;
+
+    useEffect(() => {
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                const csvData = Papa.parse(event.target.result, {
+                    header: true,
+                    dynamicTyping: true,
+                });
+                setData(csvData.data);
+            };
+            reader.readAsText(file);
+        }
+    }, [file]);
 
     return (
         <div>
             <Navbar />
             <h2>Resultado da classificação</h2>
             {file ? (
-                <a href={url} download="result.csv">Download Result File</a>
+                <div className="table-container">
+                    <table>
+                        <thead>
+                            <tr>
+                                {data.length > 0 &&
+                                    Object.keys(data[0]).map((header, index) => (
+                                        <th key={index}>{header}</th>
+                                    ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map((row, rowIndex) => (
+                                <tr key={rowIndex}>
+                                    {Object.values(row).map((value, colIndex) => (
+                                        <td key={colIndex}>{value}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             ) : (
                 <p>Nenhum arquivo disponível.</p>
             )}
+            <br />
+            <a href={url} download="result.csv">Baixar arquivo .csv</a>
         </div>
     );
 }
